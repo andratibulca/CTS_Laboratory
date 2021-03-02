ac2d799c7e5ec5dea8ea01afb7e4098b72a2971c
lenovo <lenovo@DESKTOP-RFRFC46>
2021-03-02 14:37:58 +0200

    Week2

---
+++ Week2/src/ro/ase/csie/cts/g1094/laboratory/exceptions/InsufficientFundsExceptions.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1094.laboratory.exceptions;
+
+public class InsufficientFundsExceptions extends Exception {
+	
+	public InsufficientFundsExceptions(String message) {
+		super(message);
+		}
+	public InsufficientFundsExceptions() {
+		
+	}
+
+}
