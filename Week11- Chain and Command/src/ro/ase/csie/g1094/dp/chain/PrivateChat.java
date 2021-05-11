9aedf148efe2a177ac186c63a1540ba10bc9e92d
Andra <andratibulca22@gmail.com>
2021-05-11 14:32:04 +0300

    Implementing Chain

---
+++ Week11- Chain and Command/src/ro/ase/csie/g1094/dp/chain/PrivateChat.java
@@ -0,0 +1,19 @@
+package ro.ase.csie.g1094.dp.chain;
+
+public class PrivateChat extends ChatHandler {
+
+	@Override
+	public void processMessage(Message msg) {
+		if(!msg.destination.toLowerCase().equals("@everyone")) {		
+			System.out.println("Private message for" +msg.destination+":"+ msg.text);
+		
+		}
+		else
+		{
+			if(this.next != null) {
+				this.next.processMessage(msg);
+			}
+		}
+	}
+
+}
